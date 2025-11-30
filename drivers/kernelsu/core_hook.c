e3197d6077358826537dd37ea6d72b79d77ec6d4
Rahmat Sobrian <135594470+rahmatsobrian@users.noreply.github.com>
2025-11-30 10:49:49 +0700

    Change log level format in sys_umount

--- drivers/kernelsu/core_hook.c
+++ drivers/kernelsu/core_hook.c
@@ -166,7 +166,7 @@ static void ksu_sys_umount(const char *mnt, int flags)
 #else
 	long ret = sys_umount(usermnt, flags); // cuz asmlinkage long sys##name
 	set_fs(old_fs);
-	pr_info("sys_umount: %s code: %ld \n", mnt, ret);
+	pr_info("sys_umount: %s code: %d \n", mnt, ret);
 #endif
 	return;
 }
